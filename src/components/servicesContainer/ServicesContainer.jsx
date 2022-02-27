17d24ecad09dc75871b0404fc04df0c5b116f995
Mohammed Modather <97457702+mohdmdthr@users.noreply.github.com>
2022-02-27 17:58:27 +0200

    add services to cards

---
+++ src/components/servicesContainer/ServicesContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./servicesContainer.css";
+
+const ServicesContainer = ({ children }) => {
+  return <div className="servicesContainer">{children}</div>;
+};
+
+export default ServicesContainer;
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import IconButton from "../../components/iconButton/IconButton";
+import ServicesContainer from "../../components/servicesContainer/ServicesContainer";
 
 import add from "../../assets/img/Add card.png";
 import debitCards from "../../assets/img/debit-cards.png";
 import clock from "../../assets/img/counterclockwise.png";
 import notification from "../../assets/img/notification.png";
 import card from "../../assets/img/create card.png";
+import graduationHat from "../../assets/img/graduation-hat.png";
+import invoice from "../../assets/img/invoice (2).png";
+import petrol from "../../assets/img/petrol.png";
+import exchange from "../../assets/img/exchange (1).png";
+import phone from "../../assets/img/cell-phone.png";
+import ticket from "../../assets/img/ticket.png";
+import bank from "../../assets/img/bank (3).png";
 
 import "./home.css";
 
@@ -17,6 +25,7 @@ const Home = () => {
         <div className="home__card">
           <img src={add} alt="Add card" className="home__add" />
         </div>
+
         <div className="card__buttons">
           <IconButton icon={debitCards} />
           <IconButton icon={card} />
@@ -24,6 +33,18 @@ const Home = () => {
           <IconButton icon={notification} />
         </div>
       </main>
+      <ServicesContainer>
+        <h3 className="servicesText">Services</h3>
+        <div className="services">
+          <IconButton icon={bank} />
+          <IconButton icon={exchange} />
+          <IconButton icon={invoice} />
+          <IconButton icon={petrol} />
+          <IconButton icon={phone} />
+          <IconButton icon={ticket} />
+          <IconButton icon={graduationHat} />
+        </div>
+      </ServicesContainer>
     </>
   );
 };
